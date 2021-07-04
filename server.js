59028dec224e12bcc72fc297e17920760d531df1
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-07-04 23:14:53 +0300

    ident bot oAuth

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-console.log('hello world')
+
+const tmi = require('tmi.js');
+
+const client = new tmi.Client({
+    connection: {reconnect: true},
+	channels: [ 'karriganny' ],
+    identity: {
+		username: 'my_bot_name',
+		password: 'oauth:my_bot_token'
+	}
+});
+
+client.connect();
+
+client.on('message', (channel, tags, message, self) => {
+	// "Alca: Hello, World!"
+	console.log(`${tags['display-name']}: ${message}`);
+});
+		
