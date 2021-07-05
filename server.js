859692a74f39b7932854843d12ca29f177f8b8d3
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-07-05 12:15:57 +0300

    meh

--- server.js
+++ server.js
@@ -2,18 +2,25 @@ require('dotenv').config();
 
 const tmi = require('tmi.js');
 
+const regexpCommand = new RegExp(/^!([a-zA-Z0-9]+)(?:\W+)?(.*)?/);
+
 const client = new tmi.Client({
     connection: {reconnect: true},
 	channels: [ 'karriganny' ],
     identity: {
 		username: process.env.TWITCH_BOT_USERNAME,
-		password: 'oauth:my_bot_token'
+		password: process.env.TWITCH_OAUTH_TOKEN,
 	}
 });
 
 client.connect();
 
 client.on('message', (channel, tags, message, self) => {
+	const isNotBot = tags.username.toLowerCase() !== process.env.TWITCH_BOT_USERNAME;
+
+	if ( !isNotBot ) return
+		
+	const [raw, command, argument] = message.match(regexpCommand)
 	// "Alca: Hello, World!"
 	console.log(`${tags['display-name']}: ${message}`);
 });
